11eb47faa4511d511e303145ee1299700a222a86
Aleksandar Janicijevic <Aleksandar.Janicijevic@amd.com>
2024-10-21 18:06:05 -0400

    Fixed formatting

--- cmake/Packages.cmake
+++ cmake/Packages.cmake
@@ -706,7 +706,8 @@ install(
     COMPONENT gotcha)
 if(ROCPROFSYS_BUILD_LIBUNWIND)
     install(
-        DIRECTORY ${PROJECT_BINARY_DIR}/external/timemory/external/libunwind/install/${CMAKE_DEFAULT_INSTALL_LIBDIR}/
+        DIRECTORY
+            ${PROJECT_BINARY_DIR}/external/timemory/external/libunwind/install/${CMAKE_DEFAULT_INSTALL_LIBDIR}/
         DESTINATION ${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}
         COMPONENT libunwind
         FILES_MATCHING
