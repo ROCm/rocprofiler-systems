03af371684fd862de5e0f4fb715a9bb32d728f49
Aleksandar Janicijevic <Aleksandar.Janicijevic@amd.com>
2025-03-17 17:32:59 -0400

    Fixed formatting

--- tests/rocprof-sys-nic-perf.cmake
+++ tests/rocprof-sys-nic-perf.cmake
@@ -11,10 +11,8 @@ add_test(
     WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/tests)
 
 # prog.cfg.in will be transformed by setenv.sh to generate prog.cfg.
-configure_file(
-    ${PROJECT_SOURCE_DIR}/tests/prog.cfg.in
-    ${PROJECT_BINARY_DIR}/tests/prog.cfg.in
-    COPYONLY)
+configure_file(${PROJECT_SOURCE_DIR}/tests/prog.cfg.in
+               ${PROJECT_BINARY_DIR}/tests/prog.cfg.in COPYONLY)
 
 # Copy run-sample.sh to tests directory.
 configure_file(${PROJECT_SOURCE_DIR}/tests/run-sample.sh
@@ -22,7 +20,7 @@ configure_file(${PROJECT_SOURCE_DIR}/tests/run-sample.sh
 
 # Copy verify-proto.py to tests directory.
 configure_file(${PROJECT_SOURCE_DIR}/tests/verify_proto.py
-    ${PROJECT_BINARY_DIR}/tests/verify_proto.py COPYONLY)
+               ${PROJECT_BINARY_DIR}/tests/verify_proto.py COPYONLY)
 
 # Copy setenv.sh to test directory.
 configure_file(${PROJECT_SOURCE_DIR}/tests/setenv.sh
