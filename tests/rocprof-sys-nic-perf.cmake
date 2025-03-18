ec8a4588358a7bbd1731b1b70be32df28c823e45
Aleksandar Janicijevic <Aleksandar.Janicijevic@amd.com>
2025-03-18 12:32:41 -0400

    Fixed formatting

--- tests/rocprof-sys-nic-perf.cmake
+++ tests/rocprof-sys-nic-perf.cmake
@@ -7,7 +7,8 @@
 # run-sample.sh will run the test.
 add_test(
     NAME nic-performance-profiling
-    COMMAND ${PROJECT_BINARY_DIR}/tests/run-sample.sh $<TARGET_FILE:rocprofiler-systems-sample>
+    COMMAND ${PROJECT_BINARY_DIR}/tests/run-sample.sh
+            $<TARGET_FILE:rocprofiler-systems-sample>
     WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/tests)
 
 # prog.cfg.in will be transformed by setenv.sh to generate prog.cfg.
