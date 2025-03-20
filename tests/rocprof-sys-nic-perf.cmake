49bf55e924f8f19a0cc23ec44232cbe5114ecbca
Aleksandar Janicijevic <Aleksandar.Janicijevic@amd.com>
2025-03-20 10:15:43 -0400

    Renamed run-sample.h to run-nic-profiling.sh

--- tests/rocprof-sys-nic-perf.cmake
+++ tests/rocprof-sys-nic-perf.cmake
@@ -7,7 +7,7 @@
 # run-sample.sh will run the test.
 add_test(
     NAME nic-performance-profiling
-    COMMAND ${PROJECT_BINARY_DIR}/tests/run-sample.sh
+    COMMAND ${PROJECT_BINARY_DIR}/tests/run-nic-profiling-test.sh
             $<TARGET_FILE:rocprofiler-systems-sample>
     WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/tests)
 
@@ -15,9 +15,9 @@ add_test(
 configure_file(${PROJECT_SOURCE_DIR}/tests/prog.cfg.in
                ${PROJECT_BINARY_DIR}/tests/prog.cfg.in COPYONLY)
 
-# Copy run-sample.sh to tests directory.
-configure_file(${PROJECT_SOURCE_DIR}/tests/run-sample.sh
-               ${PROJECT_BINARY_DIR}/tests/run-sample.sh COPYONLY)
+# Copy run-nic-profiling-test.sh to tests directory.
+configure_file(${PROJECT_SOURCE_DIR}/tests/run-nic-profiling-test.sh
+               ${PROJECT_BINARY_DIR}/tests/run-nic-profiling-test.sh COPYONLY)
 
 # Copy verify-proto.py to tests directory.
 configure_file(${PROJECT_SOURCE_DIR}/tests/verify_proto.py
