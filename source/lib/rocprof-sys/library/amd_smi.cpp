1369fbe50c62122bfbf3d24756ee5bfcce5e68df
David Galiffi <David.Galiffi@amd.com>
2025-03-26 15:35:41 -0500

    Handle the AMISMI header change in v2.0.
    Allowing tool to be compilable on rocm-6.4

--- source/lib/rocprof-sys/library/amd_smi.cpp
+++ source/lib/rocprof-sys/library/amd_smi.cpp
@@ -157,8 +157,14 @@ data::sample(uint32_t _dev_id)
     ROCPROFSYS_AMDSMI_GET(get_settings(m_dev_id).temp, amdsmi_get_temp_metric,
                           sample_handle, AMDSMI_TEMPERATURE_TYPE_JUNCTION,
                           AMDSMI_TEMP_CURRENT, &m_temp);
+#if(AMDSMI_LIB_VERSION_MAJOR == 2 && AMDSMI_LIB_VERSION_MINOR == 0) ||                   \
+    (AMDSMI_LIB_VERSION_MAJOR == 25 && AMDSMI_LIB_VERSION_MINOR == 2)
     ROCPROFSYS_AMDSMI_GET(get_settings(m_dev_id).power, amdsmi_get_power_info,
-                          sample_handle, &m_power)
+                        sample_handle, 0, &m_power)
+#else
+    ROCPROFSYS_AMDSMI_GET(get_settings(m_dev_id).power, amdsmi_get_power_info,
+                        sample_handle, &m_power)
+#endif
     ROCPROFSYS_AMDSMI_GET(get_settings(m_dev_id).mem_usage, amdsmi_get_gpu_memory_usage,
                           sample_handle, AMDSMI_MEM_TYPE_VRAM, &m_mem_usage);
     ROCPROFSYS_AMDSMI_GET(get_settings(m_dev_id).vcn_activity,
