ea14bc566298f1eee38b5ccd6f02fc1f163bbe1d
David Galiffi <David.Galiffi@amd.com>
2025-03-27 14:13:38 -0400

    Log AMD-SMI LIB version

--- source/lib/rocprof-sys/library/amd_smi.cpp
+++ source/lib/rocprof-sys/library/amd_smi.cpp
@@ -83,6 +83,22 @@ is_initialized()
     return _v;
 }
 
+amdsmi_version_t&
+get_version()
+{
+    static amdsmi_version_t _v = {};
+
+    if(_v.major == 0 && _v.minor == 0)
+    {
+        auto _err = amdsmi_get_lib_version(&_v);
+        if(_err != AMDSMI_STATUS_SUCCESS)
+            ROCPROFSYS_THROW(
+                "amdsmi_get_version failed. No version information available.");
+    }
+
+    return _v;
+}
+
 void
 check_error(const char* _file, int _line, amdsmi_status_t _code, bool* _option = nullptr)
 {
@@ -159,6 +175,7 @@ data::sample(uint32_t _dev_id)
                           AMDSMI_TEMP_CURRENT, &m_temp);
 #if(AMDSMI_LIB_VERSION_MAJOR == 2 && AMDSMI_LIB_VERSION_MINOR == 0) ||                   \
     (AMDSMI_LIB_VERSION_MAJOR == 25 && AMDSMI_LIB_VERSION_MINOR == 2)
+    // This was a transient change in the AMD SMI API. It was never officially released.
     ROCPROFSYS_AMDSMI_GET(get_settings(m_dev_id).power, amdsmi_get_power_info,
                           sample_handle, 0, &m_power)
 #else
@@ -261,7 +278,7 @@ data::setup()
 bool
 data::shutdown()
 {
-    ROCPROFSYS_DEBUG("Shutting down amd-smi...\n");
+    ROCPROFSYS_VERBOSE(1, "Shutting down amd-smi...\n");
     amd_smi::set_state(State::Finalized);
     return true;
 }
@@ -458,6 +475,11 @@ setup()
                              "AMD SMI is not available. Disabling AMD SMI sampling...");
         return;
     }
+
+    amdsmi_version_t _version = get_version();
+    ROCPROFSYS_VERBOSE_F(0, "AMD SMI version: %u.%u.%u.%u - str: %s.\n", _version.year,
+                         _version.major, _version.minor, _version.release,
+                         _version.build);
 
     data::device_count = gpu::get_processor_count();
 
