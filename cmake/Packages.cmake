f86dd6b55545feeb580ac3efc9a924a5002551bd
Pranjal Swarup <pranjal.swarup@amd.com>
2025-04-17 11:00:26 -0400

    Make changes to be compatible with dyninst v13

--- cmake/Packages.cmake
+++ cmake/Packages.cmake
@@ -253,8 +253,8 @@ if(ROCPROFSYS_BUILD_DYNINST)
     rocprofiler_systems_checkout_git_submodule(
         RELATIVE_PATH external/dyninst
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        REPO_URL https://github.com/jrmadsen/dyninst.git
-        REPO_BRANCH omnitrace)
+        REPO_URL https://github.com/ROCm/dyninst.git
+        REPO_BRANCH rocprofiler-systems_dyn_update)
 
     set(DYNINST_OPTION_PREFIX ON)
     set(DYNINST_BUILD_DOCS OFF)
