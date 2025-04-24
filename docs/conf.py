5b17240c9cc3c920c83d3e424dc32bb5c2ed830a
Alex Xu <alex.xu@amd.com>
2025-04-24 10:51:33 -0400

    test

--- docs/conf.py
+++ docs/conf.py
@@ -52,5 +52,6 @@ docs_core.setup()
 docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
 docs_core.enable_api_reference()
 
+# Add a comment
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
