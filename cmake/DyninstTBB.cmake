fad9338410ca8ce6804e65d4a2d916f15e8623bd
Pranjal Swarup <pranjal.swarup@amd.com>
2025-05-01 21:20:13 -0400

    Revert to original TBB version for compatibility with workflow builds

--- cmake/DyninstTBB.cmake
+++ cmake/DyninstTBB.cmake
@@ -48,7 +48,11 @@ set(TBB_USE_DEBUG_BUILD
     CACHE BOOL "Use debug versions of TBB libraries")
 
 # Minimum version of TBB (assumes a dotted-decimal format: YYYY.XX)
-set(_tbb_min_version 2020.1)
+if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+    set(_tbb_min_version 2019.7)
+else()
+    set(_tbb_min_version 2018.6)
+endif()
 
 set(TBB_MIN_VERSION
     ${_tbb_min_version}
@@ -207,8 +211,7 @@ else()
     externalproject_add(
         TBB-External
         PREFIX ${_tbb_prefix_dir}
-        GIT_REPOSITORY https://github.com/uxlfoundation/oneTBB.git
-        GIT_TAG v${TBB_MIN_VERSION}
+        URL https://github.com/ajanicijamd/oneTBB/archive/refs/tags/v${_tbb_ver_major}.${_tbb_ver_minor}.01.tar.gz
         BUILD_IN_SOURCE 1
         CONFIGURE_COMMAND ""
         BUILD_COMMAND
