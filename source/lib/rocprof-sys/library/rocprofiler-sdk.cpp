c3ca628728bfd72be0b3e559cf918c0d15a17b0e
ajanicijamd <Aleksandar.Janicijevic@amd.com>
2025-05-08 19:02:34 -0400

    Update source/lib/rocprof-sys/library/rocprofiler-sdk.cpp
    Co-authored-by: David Galiffi <David.Galiffi@amd.com>

--- source/lib/rocprof-sys/library/rocprofiler-sdk.cpp
+++ source/lib/rocprof-sys/library/rocprofiler-sdk.cpp
@@ -942,7 +942,7 @@ tool_tracing_buffered(rocprofiler_context_id_t /*context*/,
                                                                      _corr_id);
                                 }
                             });
-                        tracing::pop_perfetto(category::rocm_memory_copy{}, "", _track,
+                        tracing::pop_perfetto(category::rocm_hip_api{}, "", _track,
                                               _end_ns);
                     }
                 }
