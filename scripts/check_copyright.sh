2f2ed820c0b7fbea67bd5d2a950f1f5d4c09997c
Kian Cossettini <Kian.Cossettini@amd.com>
2025-05-20 14:53:55 -0400

    check_copyright.sh now uses LICENSE

--- scripts/check_copyright.sh
+++ scripts/check_copyright.sh
@@ -43,49 +43,31 @@ done
 
 if [ ${#files_with_missing_copyright[@]} -ne 0 ]; then
     if [[ "$ADD_AMD_COPYRIGHT" == "1" ]]; then  
-        echo "Adding copyright notice to the missing files..."  
         for file in "${files_with_missing_copyright[@]}"; do  
             # Determine the comment style based on the file extension  
             if [[ "$file" == *.c || "$file" == *.cpp || "$file" == *.h || "$file" == *.hpp ]]; then  
-                comS="//"  # For .c, .cpp, .h, .hpp files  
+                comS="//"
             else   
-                comS="#"  # For .txt and other files  
+                comS="#"
             fi  
-    
-            # Define the full copyright notice with appropriate comment prefix  
-                            copyright_notice="$comS MIT License
-$comS
-$comS Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
-$comS
-$comS Permission is hereby granted, free of charge, to any person obtaining a copy
-$comS of this software and associated documentation files (the \"Software\"), to deal
-$comS in the Software without restriction, including without limitation the rights
-$comS to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-$comS copies of the Software, and to permit persons to whom the Software is
-$comS furnished to do so, subject to the following conditions:
-$comS
-$comS The above copyright notice and this permission notice shall be included in
-$comS all copies or substantial portions of the Software.
-$comS
-$comS THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-$comS IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-$comS FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-$comS AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-$comS LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-$comS OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-$comS THE SOFTWARE."  
-    
+
+            # Read LICENSE file and prepend comment prefix to each line
+            copyright_notice=""
+            while IFS= read -r line; do
+                copyright_notice+="$comS $line"$'\n'
+            done < LICENSE
+
             # Add the notice to the beginning of the file  
             temp_file=$(mktemp)  
             {  
-                echo -e "$copyright_notice\n"  
+                echo -e "$copyright_notice"
                 cat "$file"  
             } > "$temp_file"  
             mv "$temp_file" "$file"  
         done  
         echo "Copyright notices added."  
         exit 1
-    fi  
+    fi
     echo "The following files are missing a valid copyright notice:"
     echo ""
     for file in "${files_with_missing_copyright[@]}"; do
@@ -94,12 +76,11 @@ $comS THE SOFTWARE."
     echo ""
     echo "It may be the case that the copyright is not required by some files."
     echo "To override this check, set the environment variable ALLOW_MISSING_COPYRIGHT=1"
-    echo "To add the copyright to the files, set the environment variable ADD_AMD_COPYRIGHT=1"
+    echo "To add the copyright to all files listed above, set the environment variable ADD_AMD_COPYRIGHT=1"
     if [[ "$ALLOW_MISSING_COPYRIGHT" == "1" ]]; then
         exit 0
     fi
     exit 1
 fi
 
-! $quiet && printf -- "\033[32mDone!\033[0m\n"
 exit 0
