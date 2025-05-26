e61e31cb9946bc9de28b37d90c239f67f5c99fde
Aleksandar Janicijevic <Aleksandar.Janicijevic@amd.com>
2025-05-26 17:01:06 -0400

    Fixed formatting

--- tests/rocprof-sys-nic-perf.cmake
+++ tests/rocprof-sys-nic-perf.cmake
@@ -27,9 +27,10 @@
 # -------------------------------------------------------------------------------------- #
 
 # Get the name of the default NIC and write it to _network_interface.
-execute_process(COMMAND "${CMAKE_SOURCE_DIR}/tests/get_default_nic.sh"
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-                OUTPUT_VARIABLE _network_interface)
+execute_process(
+    COMMAND "${CMAKE_SOURCE_DIR}/tests/get_default_nic.sh"
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE _network_interface)
 
 set(_nic_perf_environment
     "${_base_environment}"
@@ -45,7 +46,8 @@ set(_nic_perf_environment
     "ROCPROFSYS_PAPI_EVENTS=net:::${_network_interface}:tx:byte net:::${_network_interface}:rx:byte net:::${_network_interface}:rx:packet net:::${_network_interface}:tx:packet"
     "ROCPROFSYS_SAMPLING_DELAY=0.05")
 
-# Set _download_url to a large file that will give rocprof-sys-sample time to collect NIC performance data.
+# Set _download_url to a large file that will give rocprof-sys-sample time to collect NIC
+# performance data.
 set(_download_url
     "https://github.com/llvm/llvm-project/releases/download/llvmorg-20.1.0/clang+llvm-20.1.0-armv7a-linux-gnueabihf.tar.gz"
     )
