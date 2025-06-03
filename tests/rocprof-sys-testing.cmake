50d0848a8f27a2cc79da3cd406dc4080b1752f1e
David Galiffi <David.Galiffi@amd.com>
2025-06-03 15:22:37 -0400

    Update test to use ROCPROFSYS_USE_RCCLP

--- tests/rocprof-sys-testing.cmake
+++ tests/rocprof-sys-testing.cmake
@@ -168,7 +168,8 @@ set(_rccl_environment
     "ROCPROFSYS_USE_PROCESS_SAMPLING=ON"
     "ROCPROFSYS_TIME_OUTPUT=OFF"
     "ROCPROFSYS_USE_PID=OFF"
-    "ROCPROFSYS_ROCM_DOMAINS=hip_runtime_api,kernel_dispatch,memory_copy,rccl_api"
+    "ROCPROFSYS_USE_RCCLP=ON"
+    "ROCPROFSYS_ROCM_DOMAINS=hip_runtime_api,kernel_dispatch,memory_copy"
     "${_test_openmp_env}"
     "${_test_library_path}")
 
