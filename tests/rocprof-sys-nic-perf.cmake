e07c0ed019bd05620aeb0a9a5fbd5dd30885bc15
ajanicijamd <Aleksandar.Janicijevic@amd.com>
2025-06-03 13:59:51 -0400

    Update tests/rocprof-sys-nic-perf.cmake
    Co-authored-by: David Galiffi <David.Galiffi@amd.com>

--- tests/rocprof-sys-nic-perf.cmake
+++ tests/rocprof-sys-nic-perf.cmake
@@ -58,7 +58,7 @@ set(_download_url
 add_test(
     NAME nic-performance
     COMMAND $<TARGET_FILE:rocprofiler-systems-sample> -- wget --no-check-certificate
-            --quiet ${_download_url} -O /tmp/rocprofiler-systems.test.bin
+            ${_download_url} -O /tmp/rocprofiler-systems.test.bin
     WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
 
 set_tests_properties(nic-performance PROPERTIES ENVIRONMENT "${_nic_perf_environment}"
