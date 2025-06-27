2cdd8454400e6f1aac822d8e9d7333c6e039a53d
Kian Cossettini <Kian.Cossettini@amd.com>
2025-06-27 14:42:03 -0400

    Restructured if clause

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -138,18 +138,16 @@ validate-combinations()
             ROCM_MAJOR=$(echo ${ROCM_VERSION} | sed 's/\./ /g' | awk '{print $1}')
             ROCM_MINOR=$(echo ${ROCM_VERSION} | sed 's/\./ /g' | awk '{print $2}')
             ROCM_MAJOR_MINOR="${ROCM_MAJOR}.${ROCM_MINOR}"
-            if [ "${ROCM_MAJOR_MINOR}" == "0.0" ] && [ "${ROCM_VERSION}" != "0.0" ]; then
-                send-error "Unsupported combination :: ${DISTRO}-${VERSION} + ROCm ${ROCM_VERSION}. See compatibility matrix for supported versions."
+            if ! [ "${ROCM_MAJOR_MINOR}" == "0.0" ] && [ "${ROCM_VERSION}" != "0.0" ]; then
+                for i in "${!MATRIX_DISTROS[@]}"; do
+                    if [[ "${MATRIX_DISTROS[i]}" == "${DISTRO}" && \
+                        "${MATRIX_VERSIONS[i]}" == "${VERSION}" && \
+                        "${MATRIX_ROCM_VERSIONS[i]}" == "${ROCM_MAJOR_MINOR}" ]]; then
+                        valid=1
+                        break
+                    fi
+                done
             fi
-
-            for i in "${!MATRIX_DISTROS[@]}"; do
-                if [[ "${MATRIX_DISTROS[i]}" == "${DISTRO}" && \
-                      "${MATRIX_VERSIONS[i]}" == "${VERSION}" && \
-                      "${MATRIX_ROCM_VERSIONS[i]}" == "${ROCM_MAJOR_MINOR}" ]]; then
-                    valid=1
-                    break
-                fi
-            done
 
             if [ ${valid} -eq 0 ]; then
                 send-error "Unsupported combination :: ${DISTRO}-${VERSION} + ROCm ${ROCM_VERSION}. See compatibility matrix for supported versions."
