94d9cfc210ffb7feb7e5ada1c7efe9ea38025c39
David Galiffi <David.Galiffi@amd.com>
2025-07-22 12:28:10 -0400

    Fix group-by test for non-ROCm builds
    Signed-off-by: David Galiffi <David.Galiffi@amd.com>

--- tests/rocprof-sys-rocm-hip-stream.cmake
+++ tests/rocprof-sys-rocm-hip-stream.cmake
@@ -30,6 +30,14 @@ find_package(ROCmVersion)
 
 message(STATUS "ROCmVersion_FULL_VERSION: ${ROCmVersion_FULL_VERSION}")
 
+if(NOT DEFINED ROCmVersion_FULL_VERSION)
+    message(
+        WARNING
+        "ROCmVersion_FULL_VERSION not found, skipping tests in ${CMAKE_CURRENT_LIST_FILE}"
+    )
+    return()
+endif()
+
 if(${ROCmVersion_FULL_VERSION} VERSION_GREATER_EQUAL "7.0")
     message(STATUS "Adding Group-By Tests")
 
